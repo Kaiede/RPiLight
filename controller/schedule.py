c5b990b00bd7c540e570371e234a3b6cec6a4e37
Adam Thayer <user@biticus.net>
2018-07-23 21:54:05 -0700

    Add Schedule Parser
    Now it is time to start getting serious, add the Schedule parser to the codebase, and start writing the preview.py script.
    
    This new test script is meant to read a hardware config, a schedule config, and then run the “Preview” mode on it. This is a custom behavior that will be added to the controller that will run through a schedule given to it, using RampBehaviors and waits.
    
    It isn’t implemented, so for now, preview.py just reports back what the schedule looks like once parsed. So far it looks pretty good.

---
+++ controller/schedule.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#	schedule.py
+#
+#	Package: PiLight
+#	Module: Controller
+#	Copyright 2018 <user@biticus.net>
+#
+
+import json
+import os
+import sets
+
+from datetime import datetime
+
+#
+# Schedule Keys
+#
+KEY_BRIGHTNESS = "brightness"
+KEY_CHANNELS = "channels"
+KEY_SCHEDULE = "schedule"
+KEY_TIME = "time"
+KEY_TOKEN = "token"
+
+
+def GetConfigurationDir():
+	scriptPath = os.path.realpath(__file__)
+	scriptDir = os.path.dirname(scriptPath)
+	configDir = os.path.join("config")
+	return configDir
+
+
+def ReadSchedule(configFile, channelTokens):
+	configDict = {}
+	configPath = os.path.join(GetConfigurationDir(), configFile)
+
+	if not os.path.isfile(configPath):
+		raise ValueError('configFile')
+
+	with open(configPath, 'r') as infile:
+		configDict = json.load(infile)
+
+	return Schedule(configDict, channelTokens)
+
+
+class Schedule:
+	# m_events - Array:Event - Array of the events in the schedule. 
+
+	def __init__(self, configDict, channelTokens):
+		channelTokenSet = sets.Set(channelTokens)
+		if not configDict.has_key(KEY_SCHEDULE):
+			raise ValueError(KEY_SCHEDULE)
+
+		self.m_events = []
+		for eventDict in configDict[KEY_SCHEDULE]:
+			event = Event(eventDict, channelTokenSet)
+			self.m_events.append(event)
+
+		return
+
+	def Events(self):
+		return self.m_events;
+
+	def __str__(self):
+		string = "%d Events: " % len(self.m_events)
+		for event in self.m_events:
+			string += "\n"
+			string += str(event)
+
+		return string
+
+
+class Event:
+	def __init__(self, eventDict, channelTokenSet):
+		if not eventDict.has_key(KEY_TIME):
+			raise ValueError(KEY_TIME)
+
+		if not eventDict.has_key(KEY_CHANNELS):
+			raise ValueError(KEY_CHANNELS)
+
+		self.m_time = datetime.strptime(eventDict[KEY_TIME], '%H:%M:%S').time()
+
+		self.m_channelValues = []
+		for channelDict in eventDict[KEY_CHANNELS]:
+			channel = ChannelValue(channelDict, channelTokenSet)
+			self.m_channelValues.append(channel)
+
+		return
+
+	def Time(self):
+		return self.m_time
+
+	def DatetimeForToday(self):
+		return datetime.combine(datetime.now(), self.m_time)
+
+	def ChannelValues(self):
+		return self.m_channelValues
+
+	def __str__(self):
+		string = str(self.m_time) + " - "
+		for value in self.m_channelValues:
+			string += "{ %s : %0.2f } " % (value.Token(), value.Brightness() * 100.0)
+			
+		return string
+
+class ChannelValue:
+	# m_token
+	# m_brightness 
+
+	def __init__(self, channelDict, channelTokenSet):
+		if not channelDict.has_key(KEY_TOKEN):
+			raise ValueError(KEY_TOKEN)
+
+		if not channelDict[KEY_TOKEN] in channelTokenSet:
+			raise ValueError(channelDict[KEY_TOKEN])
+
+		if not channelDict.has_key(KEY_BRIGHTNESS):
+			raise ValueError(KEY_BRIGHTNESS)
+
+		self.m_token = channelDict[KEY_TOKEN]
+		self.m_brightness = float(channelDict[KEY_BRIGHTNESS])
+
+	def Token(self):
+		return self.m_token
+
+	def Brightness(self):
+		return self.m_brightness
---
+++ preview.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#	preview.py
+#
+#	Package: PiLight
+#	Module: PiLight
+#	Copyright 2018 <user@biticus.net>
+#
+
+import pwm
+import sys
+import controller
+
+from datetime import datetime, timedelta
+
+
+#
+# Allow users to quickly test different configs
+#
+# Arguments: <Hardware Config File> <Schedule File>
+#
+
+if len(sys.argv) < 2:
+	targetConfig = "testConfig.json"
+else:
+	targetConfig = sys.argv[1]
+
+if len(sys.argv) < 3:
+	targetSchedule = "testSchedule.json"
+else:
+	targetSchedule = sys.argv[2]
+
+
+#
+# Initialize PWM, Read Schedule, Initialize Light Controller
+#
+pwm.StartupWithConfigFile(targetConfig)
+
+availableChannels = pwm.GetValidTokens()
+print availableChannels
+
+schedule = controller.ReadSchedule(targetSchedule, availableChannels)
+
+print schedule
+
+activeChannels = []
+for token in availableChannels:
+	channel = pwm.GetChannel(token)
+	activeChannels.append(channel)
+
+
+lightController = controller.LightController(activeChannels)
+
+
+#
+# Cleanup
+#
+lightController.Shutdown()
+pwm.Shutdown()
