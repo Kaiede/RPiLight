2be3842aa3d2d3aac01533fac21a3d24459eab5a
Adam Thayer <user@biticus.net>
2018-08-13 22:58:17 -0700

    Add libpthread-workqueue-dev to Dependencies
    Found this while setting up a clean slate Pi Zero install. Not sure how this got missed, but this dependency wasn’t getting installed, but we’d fail on a clean Raspbian Lite image because of this.
    
    Once adding this to the dependencies, the whole process on a clean Raspbian Lite looks like:
    
    - sudo apt-get install git
    - git clone <RPiLight>
    - ./install.sh full
    
    Yup, three steps once you can log into Raspbian to get an installed build. And we are now at the point where everything not handled by apt now lives in /opt, making cleanup a lot easier as well (/opt/rpilight and /opt/swift).
    
    Possibly the next step around cleanup of the install is to have a more sane layout for /opt/rpilight.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@
 #
 function install_swift_prereqs() {
 	echo "Installing Dependencies for Swift..."
-	sudo apt-get install git cmake ninja-build clang-3.8 python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev
+	sudo apt-get install git cmake ninja-build clang-3.8 python uuid-dev libicu-dev icu-devtools libbsd-dev libedit-dev libxml2-dev libsqlite3-dev swig libpython-dev libncurses5-dev pkg-config libblocksruntime-dev libcurl4-openssl-dev autoconf libtool systemtap-sdt-dev libpthread-workqueue-dev
 
 	sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-3.8 100
 	sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-3.8 100
