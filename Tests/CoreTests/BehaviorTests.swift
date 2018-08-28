f68f4d184d7e7ef488f99e64a65cc81f826430c3
Adam Thayer <user@biticus.net>
2018-08-27 19:18:33 -0700

    Swift 3 Doesn’t Have DispatchTimeInterval.never
    This broke tests further…

--- Tests/CoreTests/BehaviorTests.swift
+++ Tests/CoreTests/BehaviorTests.swift
@@ -38,8 +38,10 @@ extension DispatchTimeInterval {
             return microseconds
         case .nanoseconds(let nanoseconds):
             return nanoseconds / 1_000
+        #if swift(>=4.0)
         case .never:
             return Int.max
+        #endif
         }
     }
 }
