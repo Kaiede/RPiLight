325185e91c2bf0bee41748fc5020b88624960b57
Adam Thayer <user@biticus.net>
2018-08-30 22:27:56 -0700

    Enable Overflow Test for Linux

--- Tests/CoreTests/BehaviorTests.swift
+++ Tests/CoreTests/BehaviorTests.swift
@@ -274,6 +274,7 @@ class BehaviorTests: XCTestCase {
         ("testDefaultRefresh", testDefaultRefresh),
         ("testDefaultNextUpdate", testDefaultNextUpdate),
         ("testDefaultNextUpdateSleep", testDefaultNextUpdateSleep),
+        ("testDefaultNextUpdateLargeInterval", testDefaultNextUpdateLargeInterval),
         ("testPreviewRefresh", testPreviewRefresh),
         ("testPreviewNextUpdate", testPreviewNextUpdate)
     ]
