05b621caf4649295f823c54b051a971ec4c4e03a
Adam Thayer <user@biticus.net>
2019-01-03 13:19:51 -0800

    Break Swift 3 Builds Completely
    We don't need the old Swift 3 manifest now that we are guaranteed to never build on Swift 3.

--- Package.swift
+++ Package.swift
@@ -1,43 +1,41 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-//
-// Backport of Package(url:from:)
-//
-extension Package.Dependency {
-	public class func Package(url: String, from versionString: String) -> Package.Dependency {
-		guard let version = Version(versionString) else { fatalError() }
-		let nextMajor = Version(version.major + 1, 0, 0)
-		return Package(url: url, versions: version..<nextMajor)
-	}
-}
-
-//
-// Package
-//
 let package = Package(
     name: "RPiLight",
-    targets: [
-    	Target(
-    		name: "RPiLight",
-    		dependencies: ["Core"]
-    	),
-	Target(
-		name: "Core",
-		dependencies: ["PWM", "Logging"]
-	),
-    	Target(
-            name: "PWM",
-            dependencies: ["Logging"]
-        ),
-        Target(name: "Logging")
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .executable(
+            name: "RPiLight",
+            targets: ["RPiLight"]),
     ],
     dependencies: [
-        .Package(url: "https://github.com/Kaiede/Ephemeris.git", from: "1.0.0"),
-        .Package(url: "https://github.com/kareman/Moderator.git", from: "0.5.0"),
-        .Package(url: "https://github.com/Kaiede/PCA9685.git", from: "2.0.0"),
-        .Package(url: "https://github.com/Kaiede/SingleBoard.git", from: "0.2.0")
+        // Dependencies declare other packages that this package depends on.
+        .package(url: "https://github.com/Kaiede/Ephemeris.git", from: "1.0.0"),
+        .package(url: "https://github.com/kareman/Moderator.git", from: "0.5.0"),
+        .package(url: "https://github.com/Kaiede/PCA9685.git", from: "2.0.0"),
+        .package(url: "https://github.com/Kaiede/SingleBoard.git", from: "0.2.0")
     ],
-    swiftLanguageVersions: [3, 4]
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "RPiLight",
+            dependencies: ["Core", "Moderator"]),
+        .target(
+            name: "Core",
+            dependencies: ["PWM", "Logging", "Ephemeris"]),
+        .target(
+            name: "PWM",
+            dependencies: ["Logging", "PCA9685", "SingleBoard"]),
+        .target(name: "Logging"),
+
+        // Test Targets
+        .testTarget(
+            name: "CoreTests",
+            dependencies: ["Core"]
+        )
+    ]
 )
