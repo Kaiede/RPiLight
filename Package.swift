8aee361add68d98772483a5ce35a8ae1b0a96e47
Kaiede <41651119+Kaiede@users.noreply.github.com>
2022-01-10 11:29:30 -0800

    Use Updated SingleBoard

--- Package.swift
+++ Package.swift
@@ -18,31 +18,48 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-log.git", from: "1.0.0"),
         // Internal Dependencies
         .package(url: "https://github.com/Kaiede/Ephemeris.git", from: "1.0.2"),
-        .package(url: "https://github.com/Kaiede/PCA9685.git", from: "3.0.0"),
-        .package(url: "https://github.com/Kaiede/MCP4725.git", from: "0.1.0"),
-        .package(url: "https://github.com/Kaiede/SingleBoard.git", from: "1.0.0")
+        .package(url: "https://github.com/Kaiede/SingleBoard.git", .branch("swift-revamp"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RPiLight",
-            dependencies: ["Service", "Moderator"]),
+            dependencies: [
+                .target(name: "Service"),
+                .product(name: "Moderator", package: "Moderator")
+            ]
+        ),
         .target(
             name: "Service",
-            dependencies: ["LED", "Logging", "Ephemeris", "Yams"]),
+            dependencies: [
+                .target(name: "LED"),
+                .product(name: "Logging", package: "swift-log"),
+                .product(name: "Ephemeris", package: "Ephemeris"),
+                .product(name: "Yams", package: "Yams")
+            ]
+        ),
         .target(
             name: "LED",
-            dependencies: ["Logging", "MCP4725", "PCA9685", "SingleBoard"]),
+            dependencies: [.product(name: "Logging", package: "swift-log"),
+                           .product(name: "MCP4725", package: "SingleBoard"),
+                           .product(name: "PCA9685", package: "SingleBoard"),
+                           .product(name: "SingleBoard", package: "SingleBoard")
+                          ]
+        ),
 
         // Test Targets
         .testTarget(
             name: "ServiceTests",
-            dependencies: ["Service"]
+            dependencies: [
+                .target(name: "Service")
+            ]
         ),
         .testTarget(
             name: "LEDTests",
-            dependencies: ["LED"]
+            dependencies: [
+                .target(name: "LED")
+            ]
         )
     ]
 )
